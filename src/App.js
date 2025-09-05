d178606229b8d6d51c2b9456abd11dca9326dc0d
Beatriz-SMB <bsnbernardors@gmail.com>
2025-09-05 16:08:45 -0300

    feat: implement Mapbox integration in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYi1zb2FyZXMiLCJhIjoiY21mNzZpcGp2MDltdTJtcHZqbTFranplZiJ9.JqkcsQODC_ZY1g5XJ0IN7A';
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-52.0986, -32.0332], // starting position [lng, lat]
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      zoom: 12
+    });
+
+    return () => {
+      mapRef.current.remove();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      hello world.
-    </div>
+    <>
+      <div id='map-container' ref={mapContainerRef}/>
+    </>
   );
 }
 
