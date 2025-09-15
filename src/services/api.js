bddbb71455c5bdb53f27625ebeabee3480111814
Beatriz-SMB <bsnbernardors@gmail.com>
2025-09-15 17:08:49 -0300

    chore: import keys from env

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,9 @@
 import axios from "axios";
-const VIA_CEP_API = 'https://viacep.com.br/ws';
-const API_KEY = "AIzaSyAD-j4eMhq1cFllYNUBtZPkN6VLlo38igQ";
-const GEOCODE_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-const GEOLOCATION_API = 'https://www.googleapis.com/geolocation/v1/geolocate?key=';
-const GEO_API_KEY = 'AIzaSyB90mKL29K2EadX7Z0dN44BnHgzdLztT7M'
+
 
 export const getCep = async (cep) => {
     try {
-        const response = await axios.get(`${VIA_CEP_API}/${cep}/json/`);
+        const response = await axios.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -16,7 +12,7 @@ export const getCep = async (cep) => {
 
 export const getCoordinates = async (address) => {
     try {
-        const response = await axios.get(`${GEOCODE_API}${encodeURIComponent(address)}&key=${API_KEY}`);
+        const response = await axios.get(`${process.env.GEOCODE_API}${encodeURIComponent(address)}&key=${process.env.API_KEY}`);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -25,7 +21,7 @@ export const getCoordinates = async (address) => {
 
 export const getCurrentPosition = async () => {
     try {
-        const response = await axios.post(`${GEOLOCATION_API}${API_KEY}`);
+        const response = await axios.post(`${process.env.GEOLOCATION_API}${process.env.API_KEY}`);
         return response.data;
     } catch (error) {
         console.error(error);
