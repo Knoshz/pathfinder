1620f4084f85f76a939b7f260c6db016edbf394e
Beatriz-SMB <bsnbernardors@gmail.com>
2025-09-16 15:28:17 -0300

    fix: set correct env import

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-
 export const getCep = async (cep) => {
     try {
-        const response = await axios.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
+        const response = await axios.get(`${process.env.REACT_APP_VIA_CEP_API}/${cep}/json/`);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -12,18 +11,18 @@ export const getCep = async (cep) => {
 
 export const getCoordinates = async (address) => {
     try {
-        const response = await axios.get(`${process.env.GEOCODE_API}${encodeURIComponent(address)}&key=${process.env.API_KEY}`);
+        const response = await axios.get(`${process.env.REACT_APP_GEOCODE_API}${encodeURIComponent(address)}&key=${process.env.REACT_APP_API_KEY}`);
         return response.data;
     } catch (error) {
         console.error(error);
     }
 };
 
-export const getCurrentPosition = async () => {
-    try {
-        const response = await axios.post(`${process.env.GEOLOCATION_API}${process.env.API_KEY}`);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-};
+// export const getCurrentPosition = async () => {
+//     try {
+//         const response = await axios.post(`${process.env.REACT_APP_GEOLOCATION_API}${process.env.REACT_APP_API_KEY}`);
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
